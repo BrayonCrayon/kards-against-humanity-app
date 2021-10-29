e769d6a9080849f1a6fbe98468e72510f8c637d0
bradyderoy <oR57$!^6yZbx>
2021-10-29 16:12:30 -0400

    refactor tests

--- src/Components/ExpansionCard.spec.tsx
+++ src/Components/ExpansionCard.spec.tsx
@@ -2,37 +2,26 @@ import { render, waitFor } from "@testing-library/react";
 import ExpansionCard from "./ExpansionCard";
 
 describe("Expansion Cards", function () {
-  it("has a checkbox", () => {
-    const expansionId = 1;
-    const wrapper = render(
-      <ExpansionCard
-        id={expansionId}
-        checked
-        name="Some good expansion"
-        onToggle={() => {}}
-      />
-    );
+  const expansionId = 1;
+  const isChecked = true;
+  const wrapper = render(
+    <ExpansionCard
+      id={expansionId}
+      checked={isChecked}
+      name="Some good expansion"
+      onToggle={() => {}}
+    />
+  );
+  const checkbox = wrapper.queryByTestId(
+    `checkbox-${expansionId}`
+  ) as HTMLInputElement;
 
-    const checkbox = wrapper.queryByTestId(`checkbox-${expansionId}`);
+  it("has a checkbox", () => {
     expect(checkbox).not.toBeNull();
-    expect(checkbox?.getAttribute("type")).toBe("checkbox");
+    expect(checkbox.type).toBe("checkbox");
   });
 
-  it("is checked when selected", () => {
-    const expansionId = 1;
-    const isChecked = true;
-    const wrapper = render(
-      <ExpansionCard
-        id={expansionId}
-        checked={isChecked}
-        name="Some good expansion"
-        onToggle={() => {}}
-      />
-    );
-
-    const checkbox = wrapper.queryByTestId(
-      `checkbox-${expansionId}`
-    ) as HTMLInputElement;
+  it("matches the checked property of the Expansion Card", () => {
     expect(checkbox.checked).toBe(isChecked);
   });
 });
