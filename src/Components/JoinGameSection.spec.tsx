132bf9888f6fa7030e5a4e4d72227e846acfdc34
bradyderoy <oR57$!^6yZbx>
2021-11-11 16:51:08 -0500

    WIP: adding join game form

--- src/Components/JoinGameSection.spec.tsx
+++ src/Components/JoinGameSection.spec.tsx
@@ -1,9 +1,20 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render, wait, waitFor } from "@testing-library/react";
 import { GameContext, initialState } from "../State/Game/GameContext";
 import GamePage from "../Pages/GamePage";
 import { CreateGamePage } from "../Pages/CreateGamePage";
+import userEvent from "@testing-library/user-event";
+import { apiClient } from "../Api/apiClient";
+import { gameStateExampleResponse } from "../Api/fixtures/gameStateExampleResponse";
+
+jest.mock("../Api/apiClient");
+
+const mockedAxios = apiClient as jest.Mocked<typeof apiClient>;
 
 describe("JoinGameSection", () => {
+  beforeEach(() => {
+    mockedAxios.post.mockResolvedValue({ gameStateExampleResponse });
+  });
+
   it("renders", () => {
     const wrapper = render(
       <GameContext.Provider value={initialState}>
@@ -14,12 +25,27 @@ describe("JoinGameSection", () => {
     expect(wrapper.queryByTestId("join-game-section")).not.toBeNull();
   });
 
-  it("Check if form is displayed", () => {
+  it("Check if form is displayed", async () => {
     const wrapper = render(
       <GameContext.Provider value={initialState}>
         <CreateGamePage />
       </GameContext.Provider>
     );
     expect(wrapper.queryByTestId("join-game-form")).not.toBeNull();
+
+    const nameInput = wrapper.getByTestId("join-game-name-input");
+    const codeInput = wrapper.getByTestId("join-game-code-input");
+
+    fireEvent.change(nameInput, { target: { value: "joe" } });
+    fireEvent.change(codeInput, { target: { value: "1234" } });
+
+    const submit = wrapper.getByTestId("join-game-form-submit");
+    userEvent.click(submit);
+
+    await waitFor(() => {
+      expect(mockedAxios.post).toHaveBeenCalledWith("/api/game/1234/join", {
+        name: "joe",
+      });
+    });
   });
 });
--- src/Components/JoinGameSection.tsx
+++ src/Components/JoinGameSection.tsx
@@ -3,7 +3,37 @@ import React from "react";
 const JoinGameSection: React.FC = () => {
   return (
     <div data-testid="join-game-section">
-      <form data-testid="join-game-form"></form>
+      <form
+        data-testid="join-game-form"
+        onSubmit={submitToApi}
+        className="flex w-1/3 flex-col p-4 shadow-lg rounded border"
+      >
+        <label className="mb-4 pl-2 mt-4">
+          Name:
+          <input
+            type="text"
+            data-testid="join-game-name-input"
+            name="name"
+            className="border-2 rounded shadow ml-2 px-2"
+          />
+        </label>
+        <label className="mb-4 pl-2 mt-4">
+          Code:
+          <input
+            type="text"
+            data-testid="join-game-code-input"
+            name="code"
+            className="border-2 rounded shadow ml-2 px-2"
+          />
+        </label>
+
+        <button
+          data-testid="join-game-form-submit"
+          className="bg-gray-300 p-2 text-gray-900 font-semibold rounded shadow mt-4 hover:bg-gray-200 "
+        >
+          Enter game
+        </button>
+      </form>
     </div>
   );
 };
