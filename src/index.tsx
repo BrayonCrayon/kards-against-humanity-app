6df97ed159b782862b26e638af4d3ac991f2bd8a
bradyderoy <oR57$!^6yZbx>
2021-11-23 17:03:32 -0500

    WIP

--- src/index.tsx
+++ src/index.tsx
@@ -5,29 +5,25 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import GameContextProvider from "./State/Game/GameContextProvider";
 
-// import Echo from 'laravel-echo';
+import Echo from "laravel-echo";
 
 // @ts-ignore
-// window.Pusher = require('pusher-js');
-//
-// // @ts-ignore
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: '80e06980f526e21fc058',
-//     cluster: 'ad996698bd4ed48f496c',
-//     forceTLS: true,
-//     // host: 'http://127.0.0.1:6001',
-//     // broadcaster: 'socket.io',
-//     // client: socketio,
-//     transports: ['websocket', 'polling', 'flashsocket']
-// });
-//
-// //echo.connect();
-// // @ts-ignore
-// window.Echo.channel('example')
-//     .listen('my-event', () => {
-//     console.log("Heard an event");
-// });
+window.Pusher = require("pusher-js");
+
+// @ts-ignore
+window.Echo = new Echo({
+  broadcaster: "pusher",
+  key: "80e06980f526e21fc058",
+  cluster: "us2",
+  forceTLS: true,
+  authEndpoint: "http://localhost:8080/broadcasting/auth",
+});
+
+//echo.connect();
+// @ts-ignore
+window.Echo.private("private-game.test").listen("GameJoined", () => {
+  console.log("Heard an event");
+});
 
 ReactDOM.render(
   <React.StrictMode>
