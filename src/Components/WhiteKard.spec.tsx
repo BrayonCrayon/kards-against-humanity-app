aa690ea8886b05c67e17edb32a110d1550843c0f
bradyderoy <oR57$!^6yZbx>
2021-12-03 17:36:34 -0500

    Remove old logic test

--- src/Components/WhiteKard.spec.tsx
+++ src/Components/WhiteKard.spec.tsx
@@ -1,35 +1,16 @@
-import { render, RenderResult, waitFor } from "@testing-library/react";
+import { render, RenderResult } from "@testing-library/react";
 import { WhiteKard } from "./WhiteKard";
-import userEvent from "@testing-library/user-event";
 import { whiteCardFixture } from "../Api/fixtures/whiteCardFixture";
 
 const card = whiteCardFixture[0];
-const whiteCardTestId = `white-card-${card.id}`;
 
 const selectableWhiteKardRender = (): RenderResult => {
   return render(<WhiteKard card={card} />);
-};
-
-const notSelectableWhiteKardRender = (): RenderResult => {
-  return render(<WhiteKard card={card} disable={true} />);
 };
 
 describe("Kards", function () {
   it("can render a card", () => {
     const { getByText } = selectableWhiteKardRender();
     getByText(card.text);
-  });
-
-  it("does not allow white kard to be selected when disabled", async () => {
-    const wrapper = notSelectableWhiteKardRender();
-
-    userEvent.click(wrapper.getByTestId(whiteCardTestId));
-
-    await waitFor(() => {
-      expect(wrapper.getByTestId(whiteCardTestId)).not.toHaveClass("border-4");
-      expect(wrapper.getByTestId(whiteCardTestId)).not.toHaveClass(
-        "border-blue-400"
-      );
-    });
   });
 });
