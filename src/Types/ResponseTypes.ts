ba306b1a8d1114c56ac8d80f91f8728318c9207d
bradyderoy <oR57$!^6yZbx>
2022-01-26 15:06:38 -0500

    Remove selected value in submitted card response

--- src/Types/ResponseTypes.ts
+++ src/Types/ResponseTypes.ts
@@ -1,8 +1,15 @@
 import { IWhiteCard } from "./WhiteCard";
 
+export interface SubmittedCard extends Partial<IWhiteCard> {
+  id: number;
+  text: string;
+  expansion_id: number;
+  order: number;
+}
+
 export interface PlayerSubmittedCard {
   user_id: number;
-  submitted_cards: Array<IWhiteCard>;
+  submitted_cards: Array<SubmittedCard>;
 }
 
 export interface SubmittedCardsResponse {
