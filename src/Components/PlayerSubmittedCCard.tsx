c5ca513eb33f5c8c8d79f3b2ac8367a022cdb7e4
bradyderoy <oR57$!^6yZbx>
2022-01-30 10:34:26 -0500

    Extract player submissions

---
+++ src/Components/PlayerSubmittedCCard.tsx
@@ -0,0 +1,56 @@
+import { FC, useCallback, useContext, useMemo } from "react";
+import { PlayerSubmittedCard } from "../Types/ResponseTypes";
+import { GameContext } from "../State/Game/GameContext";
+import { useVote } from "../State/Vote/VoteContext";
+import { SELECT_WINNER } from "../State/Vote/VoteActions";
+
+interface PlayerSubmittedCCardProps {
+  playerSubmission: PlayerSubmittedCard;
+}
+
+export const PlayerSubmittedCCard: FC<PlayerSubmittedCCardProps> = ({
+  playerSubmission: { user_id, submitted_cards },
+}) => {
+  const { blackCard } = useContext(GameContext);
+  const {
+    state: { selectedPlayerId },
+    dispatch,
+  } = useVote();
+
+  const playerResponse = useMemo(() => {
+    let blackCardText = blackCard.text;
+    submitted_cards
+      .sort((left, right) => left.order - right.order)
+      .forEach((card) => {
+        if (blackCardText.indexOf("_", 0) < 0) return;
+
+        blackCardText = blackCardText.replace(
+          "_",
+          card.text.replace(/\.$/, "")
+        );
+      });
+
+    return blackCardText;
+  }, [blackCard, submitted_cards]);
+
+  const selectCard = useCallback(() => {
+    dispatch({ type: SELECT_WINNER, payload: { userId: user_id } });
+  }, [user_id, dispatch]);
+
+  return (
+    <div
+      className={`bg-black text-white rounded shadow-md border p-8 
+          ${user_id === selectedPlayerId ? "border-blue-400" : "border-black"}
+      `}
+      data-testid={`player-submitted-response-${user_id}`}
+      onClick={selectCard}
+    >
+      <div
+        className="text-xl md:text-3xl font-weight-800"
+        data-testid={`player-card-response-${user_id}`}
+      >
+        <span>{playerResponse}</span>
+      </div>
+    </div>
+  );
+};
--- src/Components/VotingSection.tsx
+++ src/Components/VotingSection.tsx
@@ -1,15 +1,14 @@
 import { FC, useCallback, useContext, useEffect, useState } from "react";
 import { apiClient } from "../Api/apiClient";
 import { GameContext } from "../State/Game/GameContext";
-import { PlayerSubmittedCard, SubmittedCard } from "../Types/ResponseTypes";
+import { PlayerSubmittedCard } from "../Types/ResponseTypes";
 import { useVote } from "../State/Vote/VoteContext";
-import { SELECT_WINNER } from "../State/Vote/VoteActions";
+import { PlayerSubmittedCCard } from "./PlayerSubmittedCCard";
 
 export const VotingSection: FC = () => {
-  const { game, blackCard } = useContext(GameContext);
+  const { game } = useContext(GameContext);
   const {
     state: { selectedPlayerId },
-    dispatch,
   } = useVote();
 
   const [submittedCards, setSubmittedCards] = useState<
@@ -32,29 +31,6 @@ export const VotingSection: FC = () => {
     getSubmittedCards();
   }, [submittedCards]);
 
-  const formPlayerResponse = useCallback(
-    (cards: Array<SubmittedCard>) => {
-      let blackCardText = blackCard.text;
-      cards
-        .sort((left, right) => left.order - right.order)
-        .forEach((card) => {
-          if (blackCardText.indexOf("_", 0) < 0) return;
-
-          blackCardText = blackCardText.replace(
-            "_",
-            card.text.replace(/\.$/, "")
-          );
-        });
-
-      return blackCardText;
-    },
-    [blackCard]
-  );
-
-  const selectCard = useCallback((userId: number) => {
-    dispatch({ type: SELECT_WINNER, payload: { userId } });
-  }, []);
-
   const submitWinner = useCallback(async () => {
     if (selectedPlayerId <= 0) return;
     try {
@@ -72,26 +48,11 @@ export const VotingSection: FC = () => {
         Submitted cards
       </div>
       <div className="grid grid-cols-1 gap-4 p-4 justify-items-center md:grid-cols-2 lg:grid-flow-col">
-        {submittedCards.map((submittedCard) => (
-          <div
-            className={`bg-black text-white rounded shadow-md border p-8 
-              ${
-                submittedCard.user_id === selectedPlayerId
-                  ? "border-blue-400"
-                  : "border-black"
-              }
-            `}
-            key={submittedCard.user_id}
-            data-testid={`player-submitted-response-${submittedCard.user_id}`}
-            onClick={() => selectCard(submittedCard.user_id)}
-          >
-            <div
-              className="text-xl md:text-3xl font-weight-800"
-              data-testid={`player-card-response-${submittedCard.user_id}`}
-            >
-              <span>{formPlayerResponse(submittedCard.submitted_cards)}</span>
-            </div>
-          </div>
+        {submittedCards.map((submission) => (
+          <PlayerSubmittedCCard
+            key={submission.user_id}
+            playerSubmission={submission}
+          />
         ))}
       </div>
       <div>
