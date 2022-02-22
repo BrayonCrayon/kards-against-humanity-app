55ee6d6aee659354072ae8e9b5fa6e45d73d09d4
bradyderoy <oR57$!^6yZbx>
2022-02-22 16:26:30 -0500

    display round winner's name in RoundWinnerModal

--- src/Components/RoundWinnerModal.spec.tsx
+++ src/Components/RoundWinnerModal.spec.tsx
@@ -4,9 +4,33 @@ import * as Vote from "../State/Vote/VoteContext";
 import { submittedCardsResponse } from "../Api/fixtures/submittedCardsResponse";
 import userEvent from "@testing-library/user-event";
 import { CLEAR_STATE } from "../State/Vote/VoteActions";
+import { gameStateAllPlayerSubmittedCardsExampleResponse } from "../Api/fixtures/gameStateAllPlayerSubmittedCardsExampleResponse";
+import { Game } from "../Types/Game";
+import { transformUser, transformUsers } from "../Types/User";
 
+const game: Game = {
+  id: gameStateAllPlayerSubmittedCardsExampleResponse.data.id,
+  name: gameStateAllPlayerSubmittedCardsExampleResponse.data.name,
+  code: gameStateAllPlayerSubmittedCardsExampleResponse.data.code,
+  judge_id: gameStateAllPlayerSubmittedCardsExampleResponse.data.judge.id,
+};
+const props = {
+  game,
+  user: transformUser(
+    gameStateAllPlayerSubmittedCardsExampleResponse.data.current_user
+  ),
+  users: transformUsers(
+    gameStateAllPlayerSubmittedCardsExampleResponse.data.users
+  ),
+  judge: transformUser(
+    gameStateAllPlayerSubmittedCardsExampleResponse.data.judge
+  ),
+  blackCard:
+    gameStateAllPlayerSubmittedCardsExampleResponse.data.current_black_card,
+  hand: gameStateAllPlayerSubmittedCardsExampleResponse.data.hand,
+};
 const renderComponent = () => {
-  return customGameVoteRender(<RoundWinnerModal />);
+  return customGameVoteRender(<RoundWinnerModal />, props);
 };
 
 describe("RoundWinnerModal", () => {
@@ -56,6 +80,25 @@ describe("RoundWinnerModal", () => {
 
     expect(
       wrapper.queryByTestId("round-winner-modal-close-button")
+    ).toBeInTheDocument();
+  });
+
+  it("will display round winners name", () => {
+    const winner = submittedCardsResponse.data[0];
+    const winnerName = props.users.find((user) => {
+      return user.id === winner.user_id;
+    })!.name;
+    jest.spyOn(Vote, "useVote").mockImplementation(() => ({
+      dispatch: jest.fn,
+      state: {
+        selectedRoundWinner: winner,
+        selectedPlayerId: 1,
+      },
+    }));
+    const wrapper = renderComponent();
+
+    expect(
+      wrapper.getByText(`The winner is: ${winnerName}`)
     ).toBeInTheDocument();
   });
 
--- src/Components/RoundWinnerModal.tsx
+++ src/Components/RoundWinnerModal.tsx
@@ -1,14 +1,28 @@
 import { useVote } from "../State/Vote/VoteContext";
 import { PlayerSubmittedCCard } from "./PlayerSubmittedCCard";
-import React, { useCallback } from "react";
+import React, { useCallback, useContext, useMemo } from "react";
 import { CLEAR_STATE } from "../State/Vote/VoteActions";
 import { Button } from "./Button";
+import { GameContext } from "../State/Game/GameContext";
 
 export function RoundWinnerModal() {
   const {
     dispatch,
     state: { selectedRoundWinner },
   } = useVote();
+
+  const { users } = useContext(GameContext);
+  const name = useMemo(() => {
+    const user = users.find((user) => {
+      return user.id === selectedRoundWinner?.user_id;
+    });
+
+    if (!user) {
+      return undefined;
+    }
+
+    return user.name;
+  }, [users, selectedRoundWinner]);
 
   const close = useCallback(() => {
     dispatch({ type: CLEAR_STATE });
@@ -22,6 +36,7 @@ export function RoundWinnerModal() {
       className="w-screen h-screen fixed top-0 left-0 flex flex-col items-center justify-center bg-white bg-opacity-75"
     >
       <div className="bg-white p-2 shadow-md border-2 rounded flex flex-col">
+        <h1 className="text-4xl text-center pb-1">The winner is: {name}</h1>
         <PlayerSubmittedCCard playerSubmission={selectedRoundWinner} />
         <Button
           text="Close"
