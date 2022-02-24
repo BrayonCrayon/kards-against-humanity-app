458ef40119119a12729d6f4433e17b3ff1112a34
bradyderoy <oR57$!^6yZbx>
2022-02-24 12:08:51 -0500

    Refactor

--- src/Hooks/Game/UseRotateGame.spec.tsx
+++ src/Hooks/Game/UseRotateGame.spec.tsx
@@ -4,39 +4,51 @@ import { renderHook } from "@testing-library/react-hooks";
 import { blackCardFixture } from "../../Api/fixtures/blackcardFixture";
 import { UseRotateGame } from "./UseRotateGame";
 
+jest.mock("../../Api/apiClient");
+const mockedAxios = apiClient as jest.Mocked<typeof apiClient>;
+
 describe("UseRotateGame", () => {
+  beforeEach(() => {
+    mockedAxios.post.mockResolvedValue({});
+  });
+
   it("will call round rotation api endpoint", async () => {
-    const spyApiClient = jest.spyOn(apiClient, "post").mockResolvedValue({});
     const gameId = gameFixture.id;
     const blackCardId = blackCardFixture.id;
     const { result } = renderHook(() => UseRotateGame());
 
     await result.current(gameId, blackCardId);
 
-    expect(spyApiClient).toHaveBeenCalledWith(`/api/game/${gameId}/rotate`, {
-      blackCardId,
-    });
+    expect(mockedAxios.post).toHaveBeenCalledWith(
+      `/api/game/${gameId}/rotate`,
+      {
+        blackCardId,
+      }
+    );
   });
+
   it("will catch api call error", async () => {
     const mockErrorMessage = {
       code: 500,
       message: "server error",
     };
+    mockedAxios.post.mockRejectedValueOnce(mockErrorMessage);
     const spyConsole = jest
       .spyOn(console, "error")
       .mockImplementation(jest.fn());
-    const spyApiClient = jest
-      .spyOn(apiClient, "post")
-      .mockRejectedValue(mockErrorMessage);
     const gameId = gameFixture.id;
     const blackCardId = blackCardFixture.id;
     const { result } = renderHook(() => UseRotateGame());
 
     await result.current(gameId, blackCardId);
 
-    expect(spyApiClient).toHaveBeenCalledWith(`/api/game/${gameId}/rotate`, {
-      blackCardId,
-    });
+    expect(mockedAxios.post).toHaveBeenCalledWith(
+      `/api/game/${gameId}/rotate`,
+      {
+        blackCardId,
+      }
+    );
     expect(spyConsole).toHaveBeenCalledWith(mockErrorMessage);
+    spyConsole.mockRestore();
   });
 });
