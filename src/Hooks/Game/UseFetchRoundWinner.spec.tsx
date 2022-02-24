c5c548ccf62a38bc06d2a20185d44261761c72d6
bradyderoy <oR57$!^6yZbx>
2022-02-24 12:04:20 -0500

    Refactor

--- src/Hooks/Game/UseFetchRoundWinner.spec.tsx
+++ src/Hooks/Game/UseFetchRoundWinner.spec.tsx
@@ -1,36 +1,59 @@
 import { renderHook } from "@testing-library/react-hooks";
 import UseFetchRoundWinner from "./UseFetchRoundWinner";
 import { apiClient } from "../../Api/apiClient";
-import * as Vote from "../../State/Vote/VoteContext";
 import { WINNER_SELECTED } from "../../State/Vote/VoteActions";
 import { PlayerSubmittedCard } from "../../Types/ResponseTypes";
 import { VoteProvider } from "../../State/Vote/VoteContext";
 import { gameFixture } from "../../Api/fixtures/gameFixture";
 
+const renderUseFetchRoundWinner = () => {
+  return renderHook(UseFetchRoundWinner, {
+    wrapper: ({ children }) => <VoteProvider>{children}</VoteProvider>,
+  });
+};
+
+const mockDispatch = jest.fn();
+
+jest.mock("../../State/Vote/VoteContext", () => {
+  return {
+    ...jest.requireActual("../../State/Vote/VoteContext"),
+    useVote: () => {
+      return {
+        state: {
+          selectedPlayerId: 0,
+          selectedRoundWinner: {
+            user_id: -1,
+            submitted_cards: [],
+          },
+        },
+        dispatch: mockDispatch,
+      };
+    },
+  };
+});
+
+jest.mock("../../Api/apiClient");
+
+const mockedAxios = apiClient as jest.Mocked<typeof apiClient>;
+
+const mockApiData: PlayerSubmittedCard = {
+  submitted_cards: [],
+  user_id: 0,
+};
+
 describe("UseFetchRoundWinner", () => {
+  beforeEach(() => {
+    mockedAxios.get.mockResolvedValue({ data: mockApiData });
+  });
+
   it("it will hit the api to fetch round winner", async () => {
-    const mockDispatch = jest.fn();
-    jest.spyOn(Vote, "useVote").mockReturnValue({
-      dispatch: mockDispatch,
-      state: {
-        selectedPlayerId: -1,
-      },
-    });
+    const gameId = gameFixture.id;
+    const { result } = renderUseFetchRoundWinner();
 
-    const { result } = renderHook(UseFetchRoundWinner, {
-      wrapper: ({ children }) => <VoteProvider>{children}</VoteProvider>,
-    });
-
-    const mockApiData: PlayerSubmittedCard = {
-      submitted_cards: [],
-      user_id: 0,
-    };
-    const getSpy = jest
-      .spyOn(apiClient, "get")
-      .mockResolvedValue({ data: mockApiData });
-    const gameId = "abcd";
     await result.current({ game_id: gameId, user_id: 2 });
-    expect(getSpy).toHaveBeenCalledWith(`/api/game/${gameId}/round/winner`);
+    expect(mockedAxios.get).toHaveBeenCalledWith(
+      `/api/game/${gameId}/round/winner`
+    );
     expect(mockDispatch).toHaveBeenCalledWith({
       type: WINNER_SELECTED,
       payload: mockApiData,
@@ -38,7 +61,6 @@ describe("UseFetchRoundWinner", () => {
   });
 
   it("will catch error if api call fails", async () => {
-    const mockDispatch = jest.fn();
     const mockErrorMessage = {
       code: 500,
       message: "server failure",
@@ -46,25 +68,14 @@ describe("UseFetchRoundWinner", () => {
     const spyConsole = jest
       .spyOn(console, "error")
       .mockImplementation(jest.fn());
-    jest.spyOn(Vote, "useVote").mockReturnValue({
-      dispatch: mockDispatch,
-      state: {
-        selectedPlayerId: -1,
-      },
-    });
-    const getSpy = jest
-      .spyOn(apiClient, "get")
-      .mockRejectedValue(mockErrorMessage);
-    const { result } = renderHook(UseFetchRoundWinner, {
-      wrapper: ({ children }) => <VoteProvider>{children}</VoteProvider>,
-    });
+    mockedAxios.get.mockRejectedValueOnce(mockErrorMessage);
     const gameId = gameFixture.id;
+    const { result } = renderUseFetchRoundWinner();
 
     await result.current({ game_id: gameId, user_id: 2 });
 
     expect(spyConsole).toHaveBeenCalledWith(mockErrorMessage);
 
     spyConsole.mockRestore();
-    getSpy.mockRestore();
   });
 });
