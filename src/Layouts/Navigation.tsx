8d8419f2a5c619eabb3eea561c47f2e850e6d46c
bradyderoy <oR57$!^6yZbx>
2022-03-03 17:14:08 -0500

    Support mobile on navigation

--- src/Layouts/Navigation.tsx
+++ src/Layouts/Navigation.tsx
@@ -4,11 +4,14 @@ import { Link } from "react-router-dom";
 const Navigation: React.FC = () => {
   return (
     <nav className="flex w-full py-4 border-b-2 border-gray-300 shadow-md ">
-      <div className="text-4xl w-1/4 self-center font-semibold text-center">
+      <div className="text-4xl w-1/2 self-center font-semibold text-center pl-6 md:text-left md:w-1/5">
         Kards
       </div>
-      <Link to="/" className="text-lg self-end text-black hover:text-gray-600">
-        Play
+      <Link
+        to="/"
+        className="text-lg self-end text-black hover:text-gray-600 text-center font-semibold w-1/2 md:text-left md:w-auto"
+      >
+        Games
       </Link>
     </nav>
   );
