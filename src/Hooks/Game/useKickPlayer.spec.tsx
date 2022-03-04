f76278ee862a765dc8a83b6949314efae57871ac
bradyderoy <oR57$!^6yZbx>
2022-03-04 15:59:11 -0500

    WIP

---
+++ src/Hooks/Game/useKickPlayer.spec.tsx
@@ -0,0 +1,17 @@
+import { renderHook } from "@testing-library/react-hooks";
+import useKickPlayer from "./useKickPlayer";
+import { VoteProvider } from "../../State/Vote/VoteContext";
+import { apiClient } from "../../Api/apiClient";
+
+const renderUseKickPlayer = () => {
+  return renderHook(useKickPlayer, {
+    wrapper: ({ children }) => <VoteProvider>{children}</VoteProvider>,
+  });
+};
+
+jest.mock("../../Api/apiClient");
+const mockedAxios = apiClient as jest.Mocked<typeof apiClient>;
+
+describe("useKickPlayer", () => {
+  it("will call api to kick player", () => {});
+});
---
+++ src/Hooks/Game/useKickPlayer.tsx
@@ -0,0 +1,9 @@
+import { useCallback } from "react";
+
+function useKickPlayer() {
+  const kickPlayer = useCallback(() => {}, []);
+
+  return kickPlayer;
+}
+
+export default useKickPlayer;
