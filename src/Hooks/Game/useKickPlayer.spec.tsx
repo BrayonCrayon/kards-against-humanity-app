7fcd24fcf0a1de59b40c22e9596e8cad237502ec
bradyderoy <braydonalan@gmail.com>
2022-03-05 08:05:52 -0500

    Call kick player api endpoint

--- src/Hooks/Game/useKickPlayer.spec.tsx
+++ src/Hooks/Game/useKickPlayer.spec.tsx
@@ -2,6 +2,7 @@ import { renderHook } from "@testing-library/react-hooks";
 import useKickPlayer from "./useKickPlayer";
 import { VoteProvider } from "../../State/Vote/VoteContext";
 import { apiClient } from "../../Api/apiClient";
+import {gameFixture} from "../../Api/fixtures/gameFixture";
 
 const renderUseKickPlayer = () => {
   return renderHook(useKickPlayer, {
@@ -13,6 +14,14 @@ jest.mock("../../Api/apiClient");
 const mockedAxios = apiClient as jest.Mocked<typeof apiClient>;
 
 describe("useKickPlayer", () => {
-  it.todo("will call api to kick player", () => {});
+  it("will call api to kick player", async () => {
+    const {result} = renderUseKickPlayer();
+    const gameId = gameFixture.id;
+    const userId = 1;
+
+    await result.current(gameId, userId);
+
+    expect(mockedAxios.post).toHaveBeenCalledWith(`/api/game/${gameId}/player/${userId}/kick`);
+  });
   it.todo("will catch error if call to api fails");
 });
--- src/Hooks/Game/useKickPlayer.tsx
+++ src/Hooks/Game/useKickPlayer.tsx
@@ -1,7 +1,10 @@
 import { useCallback } from "react";
+import {apiClient} from "../../Api/apiClient";
 
 function useKickPlayer() {
-  const kickPlayer = useCallback(() => {}, []);
+  const kickPlayer = useCallback(async (gameId: string, userId: number) => {
+    await apiClient.post(`/api/game/${gameId}/player/${userId}/kick`);
+  }, []);
 
   return kickPlayer;
 }
