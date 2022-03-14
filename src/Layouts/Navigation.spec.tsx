f4ce208508114e6057389583fa9ce9cf86e09acb
bradyderoy <oR57$!^6yZbx>
2022-03-14 16:55:16 -0400

    Design Tweaks - Remove total count from sidebar - Refactor sidebar to have a consistent sizes on bigger screens - Refactor expansion card checkbox position - Adjust positioning for card count - Update navigation to have links that point to different pages - Only show join/create game links on their respectful pages

--- src/Layouts/Navigation.spec.tsx
+++ src/Layouts/Navigation.spec.tsx
@@ -1,23 +1,38 @@
-import { spyOnUseGame } from "Tests/testHelpers";
-import { gameStateExampleResponse } from "Api/fixtures/gameStateExampleResponse";
 import { kardsRender } from "Tests/testRenders";
 import Navigation from "Layouts/Navigation";
-
-const {data: {game, blackCard}} = gameStateExampleResponse;
+import { useLocation } from "react-router-dom";
 
 const renderComponent = () => {
   return kardsRender(<Navigation />);
 }
 
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useLocation: jest.fn()
+}));
+
+const mockedUseLocation = useLocation as jest.Mocked<typeof useLocation>;
+
 describe("Navigation", () => {
   it("will show navigation", () => {
+    // @ts-ignore
+    mockedUseLocation.mockImplementation(() => ({ pathname: '' }));
     const wrapper = renderComponent();
     expect(wrapper.container.querySelector('nav')).toBeInTheDocument();
   });
 
   it("will not show navigation when user is playing", () => {
-    spyOnUseGame(jest.fn(), { game, blackCard });
+    // @ts-ignore
+    mockedUseLocation.mockImplementation(() => ({ pathname: '/game/asjdfkhjaskdfhkasdjf' }))
     const wrapper = renderComponent();
     expect(wrapper.container.querySelector('nav')).not.toBeInTheDocument();
+  });
+
+  it("will not show join game link when on the create page", () => {
+    // @ts-ignore
+    mockedUseLocation.mockImplementation(() => ({ pathname: '/create' }))
+    const wrapper = renderComponent();
+
+    expect(wrapper.container.querySelector('a[href="/create"]')).not.toBeInTheDocument();
   });
 })
