49781f7bfbbc449e1b07e5aac78d9a0f9b00615e
bradyderoy <oR57$!^6yZbx>
2022-03-18 12:58:54 -0400

    Will add winners text when the black card is a question

--- src/Api/fixtures/blackcardFixture.ts
+++ src/Api/fixtures/blackcardFixture.ts
@@ -3,6 +3,6 @@ import { BlackCard } from "../../Types/BlackCard";
 export const blackCardFixture: BlackCard = {
   id: 1234,
   pick: 2,
-  text: "_____ is what you tell cheap hookers.",
+  text: "_ is what you tell cheap hookers. _",
   expansion_id: 69,
 };
