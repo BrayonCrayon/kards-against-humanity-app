edf122935ab7ce4934093104ba93c18d8b5f3810
bradyderoy <oR57$!^6yZbx>
2022-04-03 09:42:13 -0400

    Show correct percentage of users that have submitted

--- src/Components/PlayerNotificationBar.spec.tsx
+++ src/Components/PlayerNotificationBar.spec.tsx
@@ -23,7 +23,7 @@ describe("PlayerNotificationBar", () => {
   });
 
   it("will show zero out of zero if only the judge is in the game", () => {
-    const wrapper = renderComponent();
+    const wrapper = renderComponent([]);
 
     expect(wrapper.queryByTestId("player-submitted-info")).toBeInTheDocument();
     expect(wrapper.queryByTestId("player-submitted-info")?.textContent).toEqual(
@@ -35,6 +35,23 @@ describe("PlayerNotificationBar", () => {
     const nonJudgePlayers = transformUsers(
       users.filter((item) => item.id !== judge.id)
     );
+    const totalSubmittedCount = nonJudgePlayers.filter(
+      (item) => item.hasSubmittedWhiteCards
+    ).length;
+
+    const wrapper = renderComponent(nonJudgePlayers);
+
+    expect(wrapper.queryByTestId("player-submitted-info")).toBeInTheDocument();
+    expect(wrapper.queryByTestId("player-submitted-info")?.textContent).toEqual(
+      `${totalSubmittedCount}/${nonJudgePlayers.length} Players Submitted`
+    );
+  });
+
+  it("will only show half of the players that have submitted their cards", () => {
+    const nonJudgePlayers = transformUsers(
+      users.filter((item) => item.id !== judge.id)
+    );
+    nonJudgePlayers[0].hasSubmittedWhiteCards = false;
     const totalSubmittedCount = nonJudgePlayers.filter(
       (item) => item.hasSubmittedWhiteCards
     ).length;
