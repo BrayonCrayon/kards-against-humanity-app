8091f3d9f76be7b26840f055fc600cb3966d0e0a
bradyderoy <oR57$!^6yZbx>
2022-04-03 09:39:54 -0400

    Show correct percentage of users that have submitted

--- src/Components/PlayerNotificationBar.spec.tsx
+++ src/Components/PlayerNotificationBar.spec.tsx
@@ -1,8 +1,19 @@
 import { kardsRender } from "../Tests/testRenders";
 import PlayerNotificationBar from "./PlayerNotificationBar";
+import { gameStateAllPlayerSubmittedCardsExampleResponse } from "../Api/fixtures/gameStateAllPlayerSubmittedCardsExampleResponse";
+import { transformUser, transformUsers, User } from "../Types/User";
 
-const renderComponent = () => {
-  return kardsRender(<PlayerNotificationBar />);
+const {
+  data: { users, judge },
+} = gameStateAllPlayerSubmittedCardsExampleResponse;
+
+const renderComponent = (
+  usersProp: User[] = transformUsers(users),
+  judgeProp: User = transformUser(judge)
+) => {
+  return kardsRender(
+    <PlayerNotificationBar users={usersProp} judge={judgeProp} />
+  );
 };
 
 describe("PlayerNotificationBar", () => {
@@ -17,6 +28,22 @@ describe("PlayerNotificationBar", () => {
     expect(wrapper.queryByTestId("player-submitted-info")).toBeInTheDocument();
     expect(wrapper.queryByTestId("player-submitted-info")?.textContent).toEqual(
       "0/0 Players Submitted"
+    );
+  });
+
+  it("will show correct amount of players that have submitted their cards", () => {
+    const nonJudgePlayers = transformUsers(
+      users.filter((item) => item.id !== judge.id)
+    );
+    const totalSubmittedCount = nonJudgePlayers.filter(
+      (item) => item.hasSubmittedWhiteCards
+    ).length;
+
+    const wrapper = renderComponent(nonJudgePlayers);
+
+    expect(wrapper.queryByTestId("player-submitted-info")).toBeInTheDocument();
+    expect(wrapper.queryByTestId("player-submitted-info")?.textContent).toEqual(
+      `${totalSubmittedCount}/${nonJudgePlayers.length} Players Submitted`
     );
   });
 });
--- src/Components/PlayerNotificationBar.tsx
+++ src/Components/PlayerNotificationBar.tsx
@@ -1,7 +1,28 @@
-const PlayerNotificationBar = () => {
+import { User } from "../Types/User";
+import { FC, useMemo } from "react";
+
+interface PlayerNotificationBarProps {
+  users: User[];
+  judge: User;
+}
+
+const PlayerNotificationBar: FC<PlayerNotificationBarProps> = ({
+  users,
+  judge,
+}) => {
+  const nonJudgeUsers = useMemo(() => {
+    return users.filter((item) => item.id !== judge.id);
+  }, [users, judge]);
+
+  const totalSubmittedAmount = useMemo(() => {
+    return nonJudgeUsers.filter((item) => item.hasSubmittedWhiteCards).length;
+  }, [nonJudgeUsers]);
+
   return (
     <div>
-      <div data-testid="player-submitted-info">0/0 Players Submitted</div>
+      <div data-testid="player-submitted-info">
+        {totalSubmittedAmount}/{nonJudgeUsers.length} Players Submitted
+      </div>
     </div>
   );
 };
