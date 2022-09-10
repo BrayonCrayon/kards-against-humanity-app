6859dd68eb13b0e993513daa1e78e570b4abd7e5
Brady Deroy <b.deroy@vehikl.com>
2022-09-10 12:19:55 -0400

    Do not show navigation when user is playing the game

--- src/Layouts/Navigation.tsx
+++ src/Layouts/Navigation.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { useGame } from "State/Game/useGame";
 
 const Navigation: React.FC = () => {
+
+  const {state} = useGame();
+
   return (
-    <nav className="flex w-full py-4 px-2 border-b-2 border-gray-300 shadow-md bg-white justify-between baseline">
-      <img src="images/full-logo.png" alt="KAH Logo" className='w-64'/>
-      <i className="fa-solid fa-burger text-4xl self-end"></i>
-    </nav>
+    <>
+    { !state.game.id &&
+      <nav className="flex w-full py-4 px-2 border-b-2 border-gray-300 shadow-md bg-white justify-between baseline">
+        <img src="images/full-logo.png" alt="KAH Logo" className='w-64'/>
+        <i className="fa-solid fa-burger text-4xl self-end"></i>
+      </nav>
+    }
+    </>
   );
 };
 
