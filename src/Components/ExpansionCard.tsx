5d253f4af51188c09073e8d97a748573b51ac313
Brady Deroy <b.deroy@vehikl.com>
2022-09-10 13:42:15 -0400

    Fix expansion selection spacing - Change text of white card count

--- src/Components/ExpansionCard.tsx
+++ src/Components/ExpansionCard.tsx
@@ -17,20 +17,20 @@ const ExpansionCard: React.FC<ExpansionCardProps> = ({
 
   return (
     <div
-      className="bg-black text-white flex py-2 mt-2.5 px-2 cursor-pointer relative hover:font-bold hover:shadow-md"
+      className="items-center bg-black text-white flex py-2 mt-2.5 px-2 cursor-pointer relative hover:font-bold hover:shadow-md"
       data-testid={`expansion-${id}`}
       onClick={() => onToggle(id)}
     >
       <KAHCheckbox
         value={checked}
-        className="bg-white text-black mr-2 h-8 w-8"
+        className="bg-white text-black mr-2 h-8 w-8 min-w-8"
       />
-      <span className="flex flex-wrap">{name}</span>
+      <span className="flex flex-wrap mb-2">{name}</span>
       <span
         role={`white-card-count-${id}`}
-        className="absolute bottom-0 right-1 text-xs"
+        className="font-normal absolute bottom-0 right-1 text-xs"
       >
-        {whiteCardCount} - White Cards
+        Cards: {whiteCardCount}
       </span>
     </div>
   );
