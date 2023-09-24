b4149efdbcec49fcdbd5d9965b1c8ec2686abcb5
BrayonDeroy <braydonalan@gmail.com>
2023-09-24 11:09:21 -0400

    Release v1.15.0 (#86)
    * Feedback (#83)
    
    - Make Create Form Smaller
    - Fix reopening timer bug
    
    Co-authored-by: rienmack <rienmack@gmail.com>
    
    * create about us page (#85)
    
    Co-authored-by: Jacob <jacobegardiner@gmail.com>
    Co-authored-by: Justin <j.bonello@vehikl.com>
    Co-authored-by: John <johncmalapit@gmail.com>
    Co-authored-by: Joel <joel.repta@gmail.com>
    Co-authored-by: Milo <m.zapolski@vehikl.com>
    
    * Release V1.15.0
    
    ---------
    
    Co-authored-by: rienmack <rienmack@gmail.com>
    Co-authored-by: Jacob <jacobegardiner@gmail.com>
    Co-authored-by: Justin <j.bonello@vehikl.com>
    Co-authored-by: John <johncmalapit@gmail.com>
    Co-authored-by: Joel <joel.repta@gmail.com>
    Co-authored-by: Milo <m.zapolski@vehikl.com>

--- src/Layouts/Navigation.tsx
+++ src/Layouts/Navigation.tsx
@@ -27,6 +27,7 @@ const Navigation: React.FC = () => {
           <img src="images/full-logo.png" alt="KAH Logo" className="w-64"/>
           <div className="hidden md:flex">
             <Link to={link.path} className="px-2 font-bold self-end">{ link.text }</Link>
+            <Link to="/about-us" className="px-2 ml-4 font-bold self-end">About KAH</Link>
           </div>
           <div onClick={() => setShow(!show)} className="text-4xl flex flex-grow items-center justify-end md:hidden">
             <i className="fa-solid fa-burger mx-2"/>
@@ -35,8 +36,9 @@ const Navigation: React.FC = () => {
         {
           show
             ?
-            <div className="flex flex-col w-full justify-center border-t border-gray-800 pt-4 md:hidden">
+            <div className="flex flex-col gap-4 w-full justify-center border-t border-gray-800 pt-4 md:hidden">
               <Link onClick={() => setShow(!show)} to={link.path} className="font-bold self-center">{ link.text }</Link>
+              <Link onClick={() => setShow(!show)} to="/about-us" className="font-bold self-center">About KAH</Link>
             </div>
             : null
         }
