4154bd2ff9e028dd1b82b7b0c84f9e1e7a079b0c
BrayonDeroy <braydonalan@gmail.com>
2023-10-02 15:36:07 -0400

    Do not reset current player hand state when updating hand state (#90)
    - Keep order and selected state of users current hand state
    - Replace fetch game state call when another player submits their cards to only refresh the players

--- src/State/Hand/HandActions.spec.ts
+++ src/State/Hand/HandActions.spec.ts
@@ -1,10 +1,12 @@
-import { SetHandAction } from "State/Hand/HandActions";
-import { whiteCardFixture } from "Api/fixtures/whiteCardFixture";
-import { initialHandState } from "State/Hand/HandState";
+import {SetHandAction} from "State/Hand/HandActions";
+import {whiteCardFixture} from "Api/fixtures/whiteCardFixture";
+import {initialHandState} from "State/Hand/HandState";
+import {whiteCardFactory} from "Tests/Factories/WhiteCardFactory";
+import {WhiteCard} from "Types/WhiteCard";
 
 
-describe('HandActions', () => {
-  it('sets the hand', () => {
+describe("HandActions", () => {
+  it("sets the hand", () => {
     const handAction = new SetHandAction(whiteCardFixture);
 
     const result = handAction.execute(initialHandState)
@@ -12,4 +14,16 @@ describe('HandActions', () => {
     expect(result.hand).toHaveLength(whiteCardFixture.length);
     expect(whiteCardFixture).toMatchObject(result.hand);
   })
+
+  it("will keep selected state if the user hasn't selected their cards", () => {
+    const payloadFromApi = Array.from({ length: 7 }).map(() => whiteCardFactory());
+    const hand = payloadFromApi.map((card: WhiteCard, idx) => {
+      return whiteCardFactory({ ...card, selected: idx < 2, order: idx < 2 ? idx + 1 : 0  });
+    });
+    const handAction = new SetHandAction(payloadFromApi);
+
+    const result = handAction.execute({ hand });
+
+    expect(result.hand).toEqual(hand);
+  });
 })
