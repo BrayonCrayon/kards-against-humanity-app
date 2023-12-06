797d929bb5bd01e08ffc8d05f5ecf04ee8f19a4e
BrayonCrayon <braydonalan@gmail.com>
2023-12-06 16:56:54 -0500

    WIP
    Co-authored-by: jacob <jacobegardiner@gmail.com>
    Co-authored-by: giang <thuygiang3004@gmail.com>
    Co-authored-by: pkpatels <p.patel@vehikl.com>
    Co-authored-by: john <johncmalapit@gmail.com>

--- src/Pages/SpectatorPage.tsx
+++ src/Pages/SpectatorPage.tsx
@@ -1,19 +1,21 @@
-import React, {useCallback, useEffect, useMemo} from "react";
+import React, {useCallback, useMemo, useState} from "react";
 import {usePlayers} from "State/Players/usePlayers";
 import {useGame} from "State/Game/useGame";
 import useFetchSpectatorState from "Hooks/Game/State/useFetchSpectatorState";
 import {useParams} from "react-router-dom";
 import useListenOnEvents from "Hooks/Helpers/useListenOnEvents";
-import SpectatePlayerList from "Components/Spectation/SpectatePlayerList";
-import {BlackKard} from "Components/BlackKard";
 import useSubmittedCards from "Hooks/Game/State/useSubmittedCards";
 import {useAuth} from "../State/Auth/useAuth";
+import {WhiteKard} from "../Components/WhiteKard";
+import {WhiteCard} from "../Types/WhiteCard";
 
 export const SpectatorPage: React.FC = () => {
   const { state: { players } } = usePlayers();
   const { state: { auth } } = useAuth();
   const { state: { game, blackCard } } = useGame();
   const { id } = useParams<{ id: string }>();
+  const whiteCard = new WhiteCard(0, "I am a white card", 0, false);
+  const [show, setShow] = useState(false);
 
   const fetchSpectatorState = useFetchSpectatorState();
   const listenOnEvents = useListenOnEvents();
@@ -29,26 +31,33 @@ export const SpectatorPage: React.FC = () => {
     await listenOnEvents(id ?? "", auth.id);
   }, [id]);
 
-  useEffect(() => {
-    if (game.id) {
-      listenOnEvents(game.id, auth.id);
-      return;
-    }
-
-    setup();
-  }, []);
-
-  useEffect(() => {
-    if (game.id && haveAllPlayersSubmitted) {
-      getSubmittedCards(game.id);
-    }
-  },[game, haveAllPlayersSubmitted]);
+  // useEffect(() => {
+  //   if (game.id) {
+  //     listenOnEvents(game.id, auth.id);
+  //     return;
+  //   }
+  //
+  //   setup();
+  // }, []);
+  //
+  // useEffect(() => {
+  //   if (game.id && haveAllPlayersSubmitted) {
+  //     getSubmittedCards(game.id);
+  //   }
+  // },[game, haveAllPlayersSubmitted]);
 
   return <div className="flex w-full h-full bg-lukewarmGray-200">
-    <div className="flex flex-grow justify-around">
-      <BlackKard card={blackCard} />
+    {/*<CSSTransition in={show} timeout={300} unmountOnExit classNames={"spectator-card-slide"}>*/}
+    {/*  <WhiteKard card={whiteCard} onClick={() => {}} />*/}
+    {/*</CSSTransition>*/}
+    <div className="spectator-card-animation">
+        <WhiteKard card={whiteCard} onClick={() => {}} />
     </div>
-    <SpectatePlayerList players={players} judgeId={game.judgeId} />
+    <button onClick={() => setShow(!show)}>Show</button>
+    {/*<div className="flex flex-grow justify-around">*/}
+    {/*  <BlackKard card={blackCard} />*/}
+    {/*</div>*/}
+    {/*<SpectatePlayerList players={players} judgeId={game.judgeId} />*/}
     {/*{ haveAllPlayersSubmitted*/}
     {/*  ? <DisplaySubmittedCard cards={submittedCards} />*/}
     {/*  : null*/}
