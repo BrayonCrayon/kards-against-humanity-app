656c48ea1a26929ca8fef69351e1c409c27d1cab
BrayonCrayon <braydonalan@gmail.com>
2024-03-18 16:25:57 -0400

    - Fix tests - Make hook support white and black cards
    Co-authored-by: Ruta Simonavitchious <missrutamentary@gmail.com>
    Co-authored-by: Giang Nguyen <thuygiang3004@gmail.com>

--- src/Hooks/Spectate/useSwitchCard.spec.tsx
+++ src/Hooks/Spectate/useSwitchCard.spec.tsx
@@ -7,56 +7,57 @@ jest.useFakeTimers()
 describe("useSwitchCard", () => {
 
     it("will to set the first card when a list of cards is passed", () => {
-        const cards = Array(6).fill(whiteCardFactory())
+        const initialCards = Array(6).fill(whiteCardFactory())
         const { result } = renderHook(useSwitchCard, {
-            initialProps: cards
+            initialProps: {initialCards}
         });
 
-        const { whiteCard } = result.current;
+        const { card } = result.current;
 
-        expect(whiteCard).toEqual(cards[0]);
+        expect(card).toEqual(initialCards[0]);
     });
 
     it("will be able to switch to the next card after 5000ms by default", async () => {
-        const cards = Array(3).fill({}).map(whiteCardFactory);
+        const initialCards = Array(3).fill({}).map(whiteCardFactory);
+        const timeout = 3000;
+
         const { result, rerender } = renderHook(useSwitchCard, {
-            initialProps: cards
+            initialProps: {initialCards, timeout}
         });
 
-        const { whiteCard } = result.current;
-        expect(whiteCard).toEqual(cards[0]);
+        const { card } = result.current;
+        expect(card).toEqual(initialCards[0]);
 
         act(() => jest.advanceTimersByTime(5000));
-        rerender();
-        const { whiteCard: secondWhiteCard } = result.current;
-        expect(secondWhiteCard).toEqual(cards[1])
+        rerender({initialCards, timeout});
+        const { card: secondCard } = result.current;
+        expect(secondCard).toEqual(initialCards[1])
 
         act(() => jest.advanceTimersByTime(5000));
-        rerender();
-        const { whiteCard: thirdWhiteCard } = result.current;
-        expect(thirdWhiteCard).toEqual(cards[2]);
+        rerender({initialCards, timeout});
+        const { card: thirdCard } = result.current;
+        expect(thirdCard).toEqual(initialCards[2]);
     });
 
-    // TODO: Tests are failing fix it!!!!!!
     it("will be able to switch to the next card based on the timeout prop", async () => {
-        const cards = Array(3).fill({}).map(whiteCardFactory);
-        const timeout = 3000
+        const initialCards = Array(3).fill({}).map(whiteCardFactory);
+        const timeout = 3000;
 
         const { result, rerender } = renderHook(useSwitchCard, {
-            initialProps: {cards, timeout}
+            initialProps: {initialCards, timeout}
         });
 
-        const { whiteCard } = result.current;
-        expect(whiteCard).toEqual(cards[0]);
+        const { card } = result.current;
+        expect(card).toEqual(initialCards[0]);
 
         act(() => jest.advanceTimersByTime(timeout));
-        rerender();
-        const { whiteCard: secondWhiteCard } = result.current;
-        expect(secondWhiteCard).toEqual(cards[1])
+        rerender({initialCards, timeout});
+        const { card: secondCard } = result.current;
+        expect(secondCard).toEqual(initialCards[1])
 
         act(() => jest.advanceTimersByTime(timeout));
-        rerender();
-        const { whiteCard: thirdWhiteCard } = result.current;
-        expect(thirdWhiteCard).toEqual(cards[2]);
+        rerender({initialCards, timeout});
+        const { card: thirdCard } = result.current;
+        expect(thirdCard).toEqual(initialCards[2]);
     });
 })
