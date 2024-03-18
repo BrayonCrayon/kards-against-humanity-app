aa66d19ffb174823a9b51c6a1d28491a68343138
BrayonCrayon <braydonalan@gmail.com>
2024-03-18 16:25:57 -0400

    - Fix tests - Make hook support white and black cards
    Co-authored-by: Ruta Simonavitchious <missrutamentary@gmail.com>
    Co-authored-by: Giang Nguyen <thuygiang3004@gmail.com>

--- src/Hooks/Spectate/useSwitchCard.tsx
+++ src/Hooks/Spectate/useSwitchCard.tsx
@@ -1,35 +1,35 @@
 import {useCallback, useEffect, useState} from "react";
 import {WhiteCard} from "Types/WhiteCard";
 import EventEmitter from "eventemitter3";
+import {BlackCard} from "Types/BlackCard";
 
-// TODO: Write tests for this hook
-// useSwitchCardProps
-type UseSwitchCard = ({ cards, timeout }: { cards: WhiteCard[], timeout: number }) => { whiteCard: WhiteCard | null }
-const useSwitchCard: UseSwitchCard = ({cards, timeout= 5000}) => {
+type Card = WhiteCard | BlackCard;
+type UseSwitchCard = ({ initialCards, timeout }: { initialCards: Card[], timeout?: number }) => { card: Card | null }
+const useSwitchCard: UseSwitchCard = ({initialCards, timeout= 5000}) => {
 
-    const [whiteCards, setWhiteCards] = useState(cards);
-    const [whiteCard, setWhiteCard] = useState<null|WhiteCard>(cards.length ? cards[0] : null);
+    const [cards] = useState(initialCards);
+    const [card, setCard] = useState<null|Card>(initialCards?.length ? initialCards[0] : null);
     const [switchCardTimeout, setSwitchCardTimeout] = useState<NodeJS.Timeout|null>(null);
-    const [events, setEvents] = useState(new EventEmitter());
+    const [events] = useState(new EventEmitter());
 
     const switchCard = useCallback(() => {
-        if(whiteCard === null) {
-            setWhiteCard(whiteCards[0]);
+        if(card === null) {
+            setCard(cards[0]);
             switchCardCallback();
             return;
         }
 
-        const playedCardIdx = whiteCards.findIndex((card) => card.id === whiteCard.id);
+        const playedCardIdx = cards.findIndex((item) => item.id === card.id);
 
-        if ((playedCardIdx + 1) > (whiteCards.length - 1)) {
-            setWhiteCard(null);
+        if ((playedCardIdx + 1) > (cards.length - 1)) {
+            setCard(null);
             setSwitchCardTimeout(null);
             return;
         }
 
         switchCardCallback();
-        setWhiteCard(whiteCards[playedCardIdx + 1]);
-    }, [whiteCard]);
+        setCard(cards[playedCardIdx + 1]);
+    }, [card]);
 
     useEffect(() => {
         if (events.listenerCount("switchCard") > 0) {
@@ -37,13 +37,13 @@ const useSwitchCard: UseSwitchCard = ({cards, timeout= 5000}) => {
         }
 
         events.on("switchCard", switchCard, this);
-    }, [events, whiteCard]);
+    }, [events, card]);
 
     const switchCardCallback = useCallback(() => {
         const timeoutId = setTimeout(() => {
             events.emit("switchCard");
             if (switchCardTimeout) clearTimeout(switchCardTimeout);
-        }, 5000)
+        }, timeout)
 
         setSwitchCardTimeout(timeoutId)
     }, [switchCardTimeout]);
@@ -53,7 +53,7 @@ const useSwitchCard: UseSwitchCard = ({cards, timeout= 5000}) => {
     }, []);
 
     return {
-        whiteCard
+        card
     }
 }
 
--- src/Pages/SpectatorPage.tsx
+++ src/Pages/SpectatorPage.tsx
@@ -1,7 +1,7 @@
-import React, {useCallback, useEffect, useState} from "react";
+import React, {useState} from "react";
 import {WhiteKard} from "../Components/WhiteKard";
 import {WhiteCard} from "../Types/WhiteCard";
-import EventEmitter from "eventemitter3";
+import useSwitchCard from "../Hooks/Spectate/useSwitchCard";
 
 export const SpectatorPage: React.FC = () => {
   // const { state: { players } } = usePlayers();
@@ -12,10 +12,6 @@ export const SpectatorPage: React.FC = () => {
   // const fetchSpectatorState = useFetchSpectatorState();
   // const listenOnEvents = useListenOnEvents();
   // const {submittedCards, getSubmittedCards} = useSubmittedCards();
-  const [whiteCard, setWhiteCard] = useState<null|WhiteCard>(null)
-
-  const [events, setEvents] = useState(new EventEmitter());
-  const [switchCardTimeout, setSwitchCardTimeout] = useState<NodeJS.Timeout|null>(null);
 
   const [tempWhiteCards] = useState([
     new WhiteCard(0, "1", 0, false),
@@ -23,44 +19,7 @@ export const SpectatorPage: React.FC = () => {
     new WhiteCard(2, "3", 0, false),
   ]);
 
-  const switchCard = useCallback(() => {
-    if(whiteCard === null) {
-      setWhiteCard(tempWhiteCards[0]);
-      switchCardCallback();
-      return;
-    }
-
-    const playedCardIdx = tempWhiteCards.findIndex((card) => card.id === whiteCard.id);
-
-    if ((playedCardIdx + 1) > (tempWhiteCards.length - 1)) {
-      setWhiteCard(null);
-      setSwitchCardTimeout(null);
-      return;
-    }
-
-    switchCardCallback();
-    setWhiteCard(tempWhiteCards[playedCardIdx + 1]);
-  }, [whiteCard]);
-
-  useEffect(() => {
-    if (events.listenerCount("switchCard") > 0) {
-      events.removeAllListeners();
-    }
-
-    events.on("switchCard", switchCard, this);
-  }, [events, whiteCard]);
-
-  const switchCardCallback = useCallback(() => {
-      const timeoutId = setTimeout(() => {
-        if (switchCardTimeout) clearTimeout(switchCardTimeout);
-      }, 5000)
-
-      setSwitchCardTimeout(timeoutId)
-  }, [switchCardTimeout]);
-
-  useEffect(() => {
-    if(!switchCardTimeout) {switchCardCallback();}
-  }, []);
+  const {whiteCard} = useSwitchCard({ cards: tempWhiteCards });
 
 //// TODO: Make sure to not remove this piece of the code
   // const haveAllPlayersSubmitted = useMemo(() => {
@@ -89,47 +48,6 @@ export const SpectatorPage: React.FC = () => {
   // },[game, haveAllPlayersSubmitted]);
 //// TODO: End of the main setup of this page
 
-  // const {setOnEnd, play} = useReadText();
-  //
-  // const renderWhiteCards = useCallback(async () => {
-  //   if (!whiteCard) return;
-  //   // for(let i = 0; i < tempWhiteCards.length; i++){
-  //   //   setWhiteCard(tempWhiteCards[i]);
-  //     await play(whiteCard.text);
-  //
-  //
-  //     // if (tempWhiteCards[i + 1]) {
-  //     //   setOnEnd(() => play(tempWhiteCards[i + 1].text))
-  //     // }
-  //   // }
-  // }, []);
-  //
-  // const setupNextCard = useCallback(() => {
-  //   if (!whiteCard) return;
-  //   setWhiteCard(null);
-  //
-  //   const idx = tempWhiteCards.findIndex((card) => card.id === whiteCard.id);
-  //
-  //   if ((idx + 1) >= tempWhiteCards.length) {
-  //     setWhiteCard(null);
-  //   }
-  //
-  //   setWhiteCard(tempWhiteCards[idx + 1]);
-  // }, [whiteCard]);
-  //
-  // useEffect(() => {
-  //   setOnEnd(() => {
-  //     setTimeout(() => {
-  //       setupNextCard();
-  //     }, 10000);
-  //   });
-  // }, [whiteCard]);
-  // useEffect(() => {
-  //   // add ify block here that ties to button press
-  //   // document.body.click()
-  //   renderWhiteCards();
-  //   // renderWhiteCards();
-  // }, [whiteCard]);
 
   return <div className="flex w-full h-full flex-col bg-lukewarmGray-200">
     <iframe className="bg-lukewarmGray-200 w-full" src="https://lottie.host/embed/07bd7bac-9b50-4440-b5e1-38a7a9bcced9/oGrv9hk7Kj.json"></iframe>
