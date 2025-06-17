043690ed12569e52429ca84103cc108161fd7fbc
BrayonDeroy <braydonalan@gmail.com>
2025-06-17 11:44:48 -0400

    Change Judge Icon (#117)
    - Add new judge indicator that will show all the time so there is no confusion on the user side of things
    - Remove un-used code
    - Update typescript config

--- src/Components/GameInformation/GameInfo.tsx
+++ src/Components/GameInformation/GameInfo.tsx
@@ -1,17 +1,14 @@
-import React, { FC, useCallback, useState } from "react";
+import React, { FC, useState } from "react";
 import { BlackKard } from "@/Components/BlackKard";
 import { usePlayers } from "@/State/Players/usePlayers";
 import PlayerNotificationBar from "@/Components/PlayerNotificationBar";
-import JudgeMessage from "@/Components/JudgeMessage";
 import { useAuth } from "@/State/Auth/useAuth";
 import { useGame } from "@/State/Game/useGame";
 import Settings from "@/Components/Sidebars/Settings";
 import ShareButton from "@/Components/Atoms/ShareButton";
 import { ShareData } from "@/Types/WebShare";
 import CopyGameCode from "@/Components/Molecules/CopyGameCode";
-import useUpdateGameSettings from "@/Hooks/Game/State/useUpdateGameSettings";
-import { useToasts } from "@/Hooks/Notification/useToasts";
-import { Location } from "@/Types/Notification";
+import JudgeIndicator from "@/Components/Molecules/JudgeIndicator";
 
 const GameInfo: FC = () => {
   const {
@@ -23,19 +20,12 @@ const GameInfo: FC = () => {
   const {
     state: { auth },
   } = useAuth();
-  const { happyToast } = useToasts();
 
   const [data] = useState<ShareData>({
     title: "Kards Against Humanity",
     text: "Come join my game!",
     url: `https://kardsagainsthumanity.ca/${game.code}`,
   });
-  const update = useUpdateGameSettings();
-
-  const updateSettings = useCallback(async (seconds: number) => {
-    await update(game.id, seconds);
-    happyToast("Game updated!", Location.CENTER);
-  }, []);
 
   return (
     <div>
@@ -49,10 +39,10 @@ const GameInfo: FC = () => {
         </div>
         <Settings className="grow md:grow-0 md:mr-5" players={players} />
       </div>
-      <div className="pb-12 mx-auto my-2 pt-4 w-full px-6 flex flex-col items-center md:w-1/2 lg:w-1/3">
+      <div className="pb-12 mx-auto my-2 pt-4 w-full px-6 flex flex-col items-center gap-y-4 md:w-1/2 lg:w-1/3">
+        {auth.id === game.judgeId && <JudgeIndicator />}
         <BlackKard card={blackCard} />
       </div>
-      <JudgeMessage user={auth} judgeId={game.judgeId} users={players} className="mt-6" />
     </div>
   );
 };
